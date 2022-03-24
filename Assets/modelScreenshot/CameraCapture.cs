8273b5e2236d61f84e11666607f39050d73ff942
hulnglei1 <hulnglei@163.com>
2022-03-24 17:45:37 +0800

    Add files via upload

---
+++ Assets/modelScreenshot/CameraCapture.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+
+using System.IO;
+
+ 
+
+///
+
+
+/// 相机截图
+
+/// huanglei 2022-03-24
+
+///
+
+
+public class CameraCapture : MonoBehaviour {
+
+ 
+
+    // 截图尺寸
+
+    public enum CaptureSize {
+
+        CameraSize,
+
+        ScreenResolution,
+
+        FixedSize
+
+    }
+
+	//目标物体
+	public GameObject allModel;
+
+    // 目标摄像机
+
+    public Camera[] targetCameraList = new Camera[5];
+
+
+    // 截图尺寸
+
+    public CaptureSize captureSize = CaptureSize.CameraSize;
+
+    // 像素尺寸
+
+    public Vector2 pixelSize;
+
+    // 保存路径
+
+    public string savePath = "StreamingAssets/";
+
+    // 文件名称
+
+    public string[] fileNameList = {"1.jpg", "2.jpg","3.jpg","4.jpg","5.jpg"};
+
+	
+
+
+ 
+
+    #if UNITY_EDITOR
+
+    private void Reset() {
+		
+		GameObject myModel = GetComponent<GameObject>();
+		
+		for(int i = 0; i < targetCameraList.Length; i++){
+			targetCameraList[i] = GetComponent<Camera>();
+		}
+
+        pixelSize = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+
+    }
+
+    #endif
+
+ 
+
+    ///保存截图
+
+
+    /// 目标摄像机
+
+    public void saveCapture() {
+
+        Vector2 size = pixelSize;
+		
+		Directory.CreateDirectory(Application.dataPath + "/" + savePath);			
+
+		
+		foreach (Transform child in allModel.transform){
+			//激活模型
+			GameObject myModel = child.gameObject;
+			myModel.SetActive(true);
+			string modelName = myModel.name;
+			
+			for(int i = 0; i < targetCameraList.Length; i++){
+				string path = Application.dataPath + "/" + savePath + modelName +"/" + fileNameList[i];	
+				Directory.CreateDirectory(Application.dataPath + "/" + savePath + modelName);			
+				saveTexture(path, capture(targetCameraList[i], (int)size.x, (int)size.y));
+			}
+
+				//禁用模型
+			myModel.SetActive(false);			
+		}
+		
+
+
+    }
+
+ 
+
+    ///相机截图
+
+
+    /// 目标相机
+
+    public static Texture2D capture(Camera camera) {
+
+        return capture(camera, Screen.width, Screen.height);
+
+    }
+
+ 
+
+    ///相机截图
+
+
+    /// 目标相机
+
+    /// 宽度
+
+    /// 高度
+
+    public static Texture2D capture(Camera camera, int width, int height) {
+		
+
+        RenderTexture rt = new RenderTexture(width, height, 0);
+
+        rt.depth = 24;
+
+        rt.antiAliasing = 8;
+
+        camera.targetTexture = rt;
+
+        camera.RenderDontRestore();
+
+        RenderTexture.active = rt;
+
+        Texture2D texture = new Texture2D(width, height, TextureFormat.ARGB32, false, true);
+
+        Rect rect = new Rect(0, 0, width, height);
+
+        texture.ReadPixels(rect, 0, 0);
+
+        texture.filterMode = FilterMode.Point;
+
+        texture.Apply();
+
+        camera.targetTexture = null;
+
+        RenderTexture.active = null;
+
+        DestroyImmediate(rt);
+		
+		
+
+        return texture;
+
+    }
+
+ 
+
+    ///保存贴图
+
+
+    /// 保存路径
+
+    /// Texture2D
+
+    public static void saveTexture(string path, Texture2D texture) {
+
+        File.WriteAllBytes(path, texture.EncodeToJPG());
+
+        #if UNITY_EDITOR
+
+        // Debug.Log("已保存截图到:" + path);
+
+
+        #endif
+
+    }
+
+
+
+
+	
+
+
+ 
+
+}
---
+++ Assets/modelScreenshot/CameraCaptureEditor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+
+ 
+
+///
+
+
+/// 相机截图 编辑器
+
+/// ZhangYu 2018-07-06
+
+///
+
+
+[CanEditMultipleObjects]
+
+[CustomEditor(typeof(CameraCapture))]
+
+public class CameraCaptureEditor : Editor {
+
+ 
+
+    public override void OnInspectorGUI() {
+
+        // 属性
+
+        CameraCapture script = (CameraCapture)target;
+
+        int selected = (int)script.captureSize;
+
+ 
+
+        // 重绘GUI
+
+        EditorGUI.BeginChangeCheck();
+		drawProperty("allModel", "目标物体");
+		
+	    drawProperty("targetCameraList", "目标像机");
+
+
+        string[] options = new string[] { "像机尺寸", "屏幕尺寸", "固定尺寸"};
+
+        selected = EditorGUILayout.Popup("截图尺寸", selected, options, GUILayout.ExpandWidth(true));
+
+        script.captureSize = (CameraCapture.CaptureSize)selected;
+
+        if (script.captureSize == CameraCapture.CaptureSize.FixedSize) {
+
+            drawProperty("pixelSize", "像素尺寸");
+
+            EditorGUILayout.HelpBox("请保持正确的宽高比！\n否则截图区域可能出现错误。", MessageType.Info);
+
+        }
+
+        drawProperty("savePath", "保存路径");
+
+		// for(int i = 0; i < 5; i++){
+	    drawProperty("fileNameList", "文件名称");
+		// }
+
+        // drawProperty("fileName1", "文件名称");
+		// drawProperty("fileName2", "文件名称");
+		// drawProperty("fileName3", "文件名称");
+		// drawProperty("fileName4", "文件名称");
+		// drawProperty("fileName5", "文件名称");
+
+ 
+
+        // 保存截图按钮
+
+        bool isPress = GUILayout.Button("保存截图", GUILayout.ExpandWidth(true));
+
+        if (isPress) script.saveCapture();
+
+        if (EditorGUI.EndChangeCheck()) serializedObject.ApplyModifiedProperties();
+
+    }
+
+ 
+
+    private void drawProperty(string property, string label) {
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty(property), new GUIContent(label), true);
+
+    }
+
+ 
+
+}
